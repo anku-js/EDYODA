111189e93dc6f496cd3fa571ab4a9155a40bbd24
Anku Kumari <ankuhere96@gmail.com>
2023-05-17 19:15:42 +0530

    debugging

--- src/components/Course.tsx
+++ src/components/Course.tsx
@@ -6,32 +6,32 @@ const Course = () => {
   const { pointers } = content;
 
   return (
-    <section className="marketing-content-container">
-      <div className="marketing-content">
-        <div className="marketing-content-heading">
-          <h1>
-            Access curated courses worth{" "}
-            <span className="strikedText">
-              <span className="slashedPrice">₹ {slashedPrice}</span>
-            </span>{" "}
-            at just <span className="currentPrice">₹ {currentPrice}</span> per
-            year!
-          </h1>
+    <section className="payments-page">
+      <div className="payments-page-wrapper">
+        <div className="marketing-content">
+          <div className="marketing-content-heading">
+            <h1>
+              Access curated courses worth{" "}
+              <span className="strikedText">
+                <span className="slashedPrice">₹ {slashedPrice}</span>
+              </span>{" "}
+              at just <span className="currentPrice">₹ {currentPrice}</span> per
+              year!
+            </h1>
+          </div>
+          <div className="plan-benefits-wrapper">
+            <ul className="plan-benefits">
+              {pointers.map(({ icon, blueText, whiteText1, whiteText2 }) => (
+                <li key={blueText}>
+                  <img src={icon} />
+                  <p className="plan-benefits-pointers">
+                    {whiteText1} <span>{blueText}</span> {whiteText2}
+                  </p>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div className="plan-benefits-wrapper">
-          <ul className="plan-benefits">
-            {pointers.map(({ icon, blueText, whiteText1, whiteText2 }) => (
-              <li key={blueText}>
-                <img src={icon} />
-                <p className="plan-benefits-pointers">
-                  {whiteText1} <span>{blueText}</span> {whiteText2}
-                </p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-      <div>
         <Form />
       </div>
     </section>
